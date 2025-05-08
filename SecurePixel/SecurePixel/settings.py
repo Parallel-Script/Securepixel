6321ead35c55b05cb00d4a9ebe087f55658d967e
Varadraj Patil <patilvaradraj18@gmail.com>
2025-05-08 19:13:58 +0530

    fix

--- SecurePixel/SecurePixel/settings.py
+++ SecurePixel/SecurePixel/settings.py
@@ -147,7 +147,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 
-
 ############################################
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
